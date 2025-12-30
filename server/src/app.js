d5f4ababf6c81bee6093e165461dfeed4549a053
KOTA NAGA SEETHA <155760169+KotaNagaSeetha@users.noreply.github.com>
2025-12-30 22:19:19 +0530

    Restrict CORS to specific origins

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,14 @@ import analyticsRoutes from "./routes/analyticsRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://fin-track-personal-finance-tracker.vercel.app/"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
